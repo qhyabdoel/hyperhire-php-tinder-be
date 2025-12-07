0cfcdb0270c22fe6df97d7eb35404adfd0b7da1e
kikicreativeans <kiki@creativeans.com>
2025-12-07 18:04:51 +0700

    update route swagger

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,3 @@ Route::get('/users/recommended', [UserController::class, 'index']);
 Route::post('/likes', [UserController::class, 'like']);
 Route::post('/dislikes', [UserController::class, 'dislike']);
 Route::get('/users/{id}/likes', [UserController::class, 'likedPeople']);
-
-Route::get('/docs', [SwaggerController::class, 'api'])->name('l5-swagger.default.api');
-Route::get('/docs/asset/{asset}', [SwaggerController::class, 'asset'])->name('l5-swagger.default.asset');
-Route::get('/docs/oauth2_callback', [SwaggerController::class, 'oauth2Callback'])->name('l5-swagger.default.oauth2_callback');
